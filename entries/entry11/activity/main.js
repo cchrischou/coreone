03fdd469a27d524657238eed3e933593f57bfc7c
cchrischou <chouc513@newschool.edu>
2024-11-26 20:26:22 -0500

    commitdemo

---
+++ entries/entry11/activity/main.js
@@ -0,0 +1,51 @@
+
+// let body = document.body;
+// let headline = document.querySelector("h1");
+// let headlines = document.querySelectorAll("h1");
+
+// console.log(headline);
+// console.log(headlines);
+// ...............................
+// let body = document.body;
+// let headline = document.querySelector("h1");
+// let headlines = document.querySelectorAll("h1");
+
+// console.log(headline);
+// console.log(headlines);
+
+// function addRed() {
+//   headline.classList.toggle("red");
+// }
+
+// headline.addEventListener("click", addRed);
+// ...........................
+
+// let body = document.body;
+// let headline = document.querySelector("h1");
+// let headlines = document.querySelectorAll("h1");
+
+// console.log(headline);
+// console.log(headlines);
+
+// function addRed() {
+
+//   headlines.forEach(function (headline) {
+//     headline.classList.add("red");
+//   })
+// }
+// addRed();
+
+
+let body = document.body;
+let headlines = document.querySelectorAll("h1");
+
+console.log(headlines);
+
+// uses forEach to loop through all of the headlines
+headlines.forEach(function(headline) {
+  // add an event listener to each individual headline
+  headline.addEventListener('click', function (event) {
+    // toggle the red class on this headline
+    headline.classList.toggle("red");
+  });
+});
